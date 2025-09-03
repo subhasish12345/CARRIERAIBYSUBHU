c3538b7e17a7e051b72da7bdd98eeffca5da4df6
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-09-03 11:54:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type = "text", ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
