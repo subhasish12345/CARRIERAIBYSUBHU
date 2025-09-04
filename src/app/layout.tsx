3284e686e52d009eb6008ee4a66723cdbafdc6b4
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-09-04 19:05:18 +0000

    ok i have i=uploaded the a logo  fie named clogo in the root i want when

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/clogo.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
