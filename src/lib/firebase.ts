f49d594a92e078e8c31117669c1f566d90d43d56
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-09-04 07:42:25 +0000

    Currently, my Firebase project only has Realtime Database enabled, but i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
     projectId: "careercompass-ai-qno3f",
     appId: "1:622318145847:web:27fe6b36c6af9910c9cab2",
-    storageBucket: "careercompass-ai-qno3f.firebasestorage.app",
+    storageBucket: "careercompass-ai-qno3f.appspot.com",
     apiKey: "AIzaSyDj6wXqbdgkCk2l3k6YrvoCqxCtzTWbCMg",
     authDomain: "careercompass-ai-qno3f.firebaseapp.com",
     messagingSenderId: "622318145847",
